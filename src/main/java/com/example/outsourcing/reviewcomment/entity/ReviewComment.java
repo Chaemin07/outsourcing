68244d9879f9ba6615169e5b4c724cfccdd746d6
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-04-23 18:01:49 +0900

    Env: Getter 추가

--- src/main/java/com/example/outsourcing/reviewcomment/entity/ReviewComment.java
+++ src/main/java/com/example/outsourcing/reviewcomment/entity/ReviewComment.java
@@ -4,7 +4,9 @@ import com.example.outsourcing.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "review_comment")
 public class ReviewComment extends BaseEntity {
