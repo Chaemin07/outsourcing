b4ef11f3e43e956e6e3322a764e3957dda66b007
HanGil <jhg9240@gmail.com>
2025-04-24 11:23:10 +0900

    Fix: 충돌 해결

--- src/main/java/com/example/outsourcing/address/entity/Address.java
+++ src/main/java/com/example/outsourcing/address/entity/Address.java
@@ -10,10 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
+@Getter
 @Table(name = "address")
 public class Address extends BaseEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   Long id;
