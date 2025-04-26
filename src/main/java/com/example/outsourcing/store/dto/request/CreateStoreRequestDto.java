315299716f1300e32aefded71b8c294d22a48064
HanGil <jhg9240@gmail.com>
2025-04-26 14:43:20 +0900

    refactor: 오픈시간, 마감시간, 전화번호 패턴 적용

--- src/main/java/com/example/outsourcing/store/dto/request/CreateStoreRequestDto.java
+++ src/main/java/com/example/outsourcing/store/dto/request/CreateStoreRequestDto.java
@@ -1,5 +1,7 @@
 package com.example.outsourcing.store.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,12 +11,18 @@ public class CreateStoreRequestDto {
 
     private final String name;
 
+    @NotBlank(message = "매장전화번호는 필수값입니다.")
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "유효한 메장전화번호 형식이 아닙니다.")
     private final String storePhoneNumber;
 
     private final Integer minOrderPrice;
 
+    @NotBlank(message = "오픈 시간은 필수값입니다.")
+    @Pattern(regexp = "^(?:[01]\\d|2[0-3]):[0-5]\\d$", message = "유효한 오픈 시간 형식이 아닙니다.")
     private final String openingTimes;
 
+    @NotBlank(message = "마감 시간은 필수값입니다.")
+    @Pattern(regexp = "^(?:[01]\\d|2[0-3]):[0-5]\\d$", message = "유효한 마감 시간 형식이 아닙니다.")
     private final String closingTimes;
 
     private final String notification;
